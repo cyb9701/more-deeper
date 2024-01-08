260a4e066cedaf8b80178b2dbcbf7f946041fa77
cyb9701 <cyb9701@gmail.com>
2024-01-08 17:33:04 +0900

    강의 폴더 제거

--- 강의/[Udemy] React 완변 가이드 with Redux, Next.js, TypeScript/09(2)-custom-hook/src/components/Tasks/TaskItem.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './TaskItem.module.css';
-
-function TaskItem(props) {
-  return <li className={classes.task}>{props.children}</li>;
-}
-
-export default TaskItem;
--- 강의/[Udemy] React 완변 가이드 with Redux, Next.js, TypeScript/09(2)-custom-hook/src/components/UI/Section.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './Section.module.css';
-
-function Section(props) {
-  return <section className={classes.section}>{props.children}</section>;
-}
-
-export default Section;
